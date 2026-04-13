8b5610057a2bf8b13790c8373bcf263752284dfc
TranThiBichTuyen1402 <ttbtuyen1402@gmail.com>
2026-04-14 02:52:42 +0700

    Update auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const khachhang = require('./models/khachhang');
+const khachhang = require('../models/khachhang');
 const passport = require('passport');
 
 // --- ĐĂNG KÝ ---
