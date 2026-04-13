e0ecfa968d3f49a41e6d16221cc8c2f5a2f33479
TranThiBichTuyen1402 <ttbtuyen1402@gmail.com>
2026-04-14 01:51:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
-}));
+})); 
 
 // KÍCH HOẠT PASSPORT
 app.use(passport.initialize());
